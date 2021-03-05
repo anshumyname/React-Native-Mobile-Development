f8426356923305b86b4728dcd74a5fee33945808
Anshuman Srivastava <srivastavaanshuman33@gmail.com>
2021-03-05 13:43:44 +0530

    social-sharing

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,24 +1,44 @@
 import React, { Component } from 'react';
-import { Card, Text, ListItem } from 'react-native-elements';
-import { View } from 'react-native';
+import { Card, Text, ListItem, Button, Icon } from 'react-native-elements';
 import { ScrollView } from 'react-native-gesture-handler';
 import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
 
-function Contact(props) {
-    return (
-        <ScrollView>
-            <Animatable.View animation="fadeInDown" duration={2000} delay={1000} useNativeDriver={true}>
-                <Card title="Contact Information">
-                    <Text>121, Clear Water Bay Road</Text>
-                    <Text>Clear Water Bay, Kowloon</Text>
-                    <Text>HONG KONG</Text>
-                    <Text>Tel: +852 1234 5678</Text>
-                    <Text>Fax: +852 8765 4321</Text>
-                    <Text>Email:confusion@food.net</Text>
-                </Card>
-            </Animatable.View>
-        </ScrollView>
-    )
+class Contact extends Component  {
+    constructor(props){
+        super(props);
+
+    }
+
+    sendMail() {
+        MailComposer.composeAsync({
+            recipients: ['confusion@food.net'],
+            subject: 'Enquiry',
+            body: 'To whom it may concern'
+        })
+    }
+    render() {
+        return (
+            <ScrollView>
+                <Animatable.View animation="fadeInDown" duration={2000} delay={1000} useNativeDriver={true}>
+                    <Card title="Contact Information">
+                        <Text>121, Clear Water Bay Road</Text>
+                        <Text>Clear Water Bay, Kowloon</Text>
+                        <Text>HONG KONG</Text>
+                        <Text>Tel: +852 1234 5678</Text>
+                        <Text>Fax: +852 8765 4321</Text>
+                        <Text>Email:confusion@food.net</Text>
+                        <Button title=" Send Email" 
+                                buttonStyle={{backgroundColor:'#512DA8', margin:10 }}  
+                                icon={<Icon name="envelope-o" type="font-awesome" color="white"></Icon>} 
+                                onPress={this.sendMail}
+                        />
+    
+                    </Card>
+                </Animatable.View>
+            </ScrollView>
+        )
+    }
 }
 
 export default Contact;
--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView, FlatList, Modal, StyleSheet, Button , Alert, PanResponder} from 'react-native';
+import { View, Text, ScrollView, FlatList, Modal, StyleSheet, Button , Alert, PanResponder, Share} from 'react-native';
 import { Card, Rating, Input} from 'react-native-elements';
 import { Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
@@ -7,6 +7,7 @@ import { baseURL } from '../shared/baseURL';
 import { postFavorite, postmyComment } from '../redux/ActionCreators';
 import { createIconSetFromFontello } from 'react-native-vector-icons';
 import * as Animatable from 'react-native-animatable';
+
 
 const mapStateToProps = state => {
     return {
@@ -69,6 +70,16 @@ function RenderDish(props) {
         }
     })
 
+    const shareDish = (title , message, url) => {
+        Share.share({
+            title: title,
+            message: title + ':' + message +' ' +url,
+            url: url
+        },{
+            dialogTitle: 'Share '+ title
+        });
+    }
+
     if (dish != null) {
         return (
             <Animatable.View animation="fadeInDown" duration={2000} delay={1000} useNativeDriver={true}
@@ -78,7 +89,7 @@ function RenderDish(props) {
                     <Text stype={{ margin: 10 }}>
                         {dish.description}
                     </Text>
-                    <View style={{ flex: 1, flexDirection: "row" }}>
+                    <View style={{ flex: 1, flexDirection: "row" , justifyContent: 'center'}}>
                         <Icon
                             raised
                             reverse
@@ -94,6 +105,15 @@ function RenderDish(props) {
                             type="font-awesome"
                             color="#512DA8"
                             onPress={() => props.toggleModal()}
+                        />
+                        <Icon 
+                            raised
+                            reverse
+                            name= 'share'
+                            type='font-awesome'
+                            color="#51D2A8"
+                            style={styles.cardItem}
+                            onPress= { () => shareDish(dish.name, dish.description, baseURL+dish.image)}
                         />
                     </View>
                 </Card>
@@ -226,6 +246,10 @@ const styles = StyleSheet.create({
         margin: 20,
         padding: 10,
     },
+    cardItem: {
+        justifyContent: 'center'
+    }
+    ,
     modalTitle: {
         fontSize: 24,
         fontWeight: 'bold',
